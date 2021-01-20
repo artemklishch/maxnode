2e7fc9671e8f5b5b7bd988b0ce2c5fd21d954fd0
Artem Klishch <artemklishch@gmail.com>
2021-01-20 14:36:56 +0200

    dsafsadfsdf

---
+++ 7/pr1/controllers/admin.js
@@ -0,0 +1,31 @@
+const Product = require("../models/product");
+
+exports.getAddProductPage = (req, res, next) => {
+  res.render("admin/add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const newProduct = new Product(title, imageUrl, description, price);
+  newProduct.save();
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  });
+};
